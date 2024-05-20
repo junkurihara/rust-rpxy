9fdb378d320cc890390e75bdee5f8ea6558da8b5
Jun Kurihara <junkurihara@users.noreply.github.com>
2024-05-21 02:11:27 +0900

    chore: deps + refactor logging code

--- rpxy-bin/src/log.rs
+++ rpxy-bin/src/log.rs
@@ -1,28 +1,30 @@
+use std::str::FromStr;
+use tracing_subscriber::{fmt, prelude::*};
+
 #[allow(unused)]
 pub use tracing::{debug, error, info, warn};
 
+/// Initialize the logger with the RUST_LOG environment variable.
 pub fn init_logger() {
-  use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+  let level_string = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
+  let level = tracing::Level::from_str(level_string.as_str()).unwrap_or(tracing::Level::INFO);
 
-  let format_layer = fmt::layer()
-    .with_line_number(false)
+  // This limits the logger to emits only this crate with any level, for included crates it will emit only INFO or above level.
+  let stdio_layer = fmt::layer()
+    .with_line_number(true)
     .with_thread_ids(false)
-    .with_target(false)
     .with_thread_names(true)
     .with_target(true)
     .with_level(true)
-    .compact();
+    .compact()
+    .with_filter(tracing_subscriber::filter::filter_fn(move |metadata| {
+      (metadata
+        .target()
+        .starts_with(env!("CARGO_PKG_NAME").replace('-', "_").as_str())
+        && metadata.level() <= &level)
+        || metadata.level() <= &tracing::Level::INFO.min(level)
+    }));
 
-  // This limits the logger to emits only proxy crate
-  let pkg_name = env!("CARGO_PKG_NAME").replace('-', "_");
-  let level_string = std::env::var(EnvFilter::DEFAULT_ENV).unwrap_or_else(|_| "info".to_string());
-  let filter_layer = EnvFilter::new(format!("{}={}", pkg_name, level_string));
-  // let filter_layer = EnvFilter::try_from_default_env()
-  //   .unwrap_or_else(|_| EnvFilter::new("info"))
-  //   .add_directive(format!("{}=trace", pkg_name).parse().unwrap());
-
-  tracing_subscriber::registry()
-    .with(format_layer)
-    .with(filter_layer)
-    .init();
+  let reg = tracing_subscriber::registry().with(stdio_layer);
+  reg.init();
 }
